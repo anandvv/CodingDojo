e562f479b7d34f70cece4ac7f965c30e8f884362
Anand V V <anand.vijay@gmail.com>
2017-10-16 13:13:13 -0700

    First JavaScript assignment!

---
+++ projects/JavaScript/IDoDeclare/script.js
@@ -0,0 +1,25 @@
+var age = 41;
+var wifesAge = 37;
+var childsAge = 1;
+
+var firstName = "Anand";
+var lastName = "Vijay";
+var address = "XXXXX Starsurf Ln, Fremont CA 94545";
+
+var isMarried = true;
+var isParent = true;
+
+var ageWhenGradparent = undefined;
+
+console.log("My Age: " + age);
+console.log("My Wife's Age: " + wifesAge);
+console.log("My Child's Age: " + childsAge);
+
+console.log("My First Name: " + firstName);
+console.log("My Last Name: " + lastName);
+console.log("My Address: " + address);
+
+console.log("Am I married: " + isMarried);
+console.log("Am I a parent: " + isParent);
+
+console.log("My age when I become a grandparent: " + ageWhenGradparent);
