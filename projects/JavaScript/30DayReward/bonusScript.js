b47ac723c4a2d46f83be93fccbd0f06193026e6e
Anand V V <anand.vijay@gmail.com>
2017-10-16 21:27:37 -0700

    Fixed the bug that forgot to add the amount earned previously.

--- projects/JavaScript/30DayReward/bonusScript.js
+++ projects/JavaScript/30DayReward/bonusScript.js
@@ -1,24 +1,30 @@
 var amount = 0.01;
+var earned = 0.01;
 var isOver10k = false;
 var isOver1Billion = false;
 var isInfinity = false;
 
 for (var i=1; i <=1031 ; i++){
+    if(i==1){
+        earned = 0.01;
+    }
     amount *= 2;
-    if(amount >= 10000 && isOver10k == false){
+    earned += amount;
+
+    if(earned >= 10000 && isOver10k == false){
         console.log('It took ', i, 'days to make $10000');
         isOver10k = true;
     }
 
-    if(amount >= 1000000000 && isOver1Billion == false){
+    if(earned >= 1000000000 && isOver1Billion == false){
         console.log('It took ', i, 'days to make $1 Billion');
         isOver1Billion = true;
     }
     
-    if(amount >= Infinity && isInfinity == false){
+    if(earned >= Infinity && isInfinity == false){
         console.log('It took ', i, 'days to make infinity');
         isInfinity = true;
     }
 }
 
-console.log("Amount after 1031 days: " + amount);
+console.log("Amount earned after 1031 days: " + earned);
