2b3261b2c1bca02bff2c59922cce67ddb7e48e52
Anand V V <anand.vijay@gmail.com>
2017-12-17 16:20:32 -0800

    Sample Django application

---
+++ projects/05-Python/Django/demo/main/apps/first_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+# Create your views here.
+def index(request):
+    response = "Hello, I am your first request!"
+    return HttpResponse(response)
+
+
